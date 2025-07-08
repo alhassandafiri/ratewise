dfdd993da1568231c48f26a412b8c18de4738fbf
Harson <hassandaf07@gmail.com>
2025-07-08 13:48:25 +0100

    amend conversion result file

--- src/components/ConversionResult/ConversionResult.jsx
+++ src/components/ConversionResult/ConversionResult.jsx
@@ -1,12 +1,39 @@
 import styles from './ConversionResult.module.css'
 
-function ConversionResult() {
+function ConversionResult({
+  isLoading,
+  amount,
+  fromCurrency,
+  toCurrency,
+  convertedAmount,
+  exchangeRate
+}) {
+
+  if (isLoading) {
+    return <div className={styles.resultContainer}>
+      <p>Retrieving exchange rate...</p>
+    </div>
+  }
+
+  if (convertedAmount === null || !amount) {
+    return <div className={styles.resultContainer}>
+      <p>Enter an amount to see the conversion.</p>
+    </div>
+  }
+
   return(
-    <section className={styles.conversionResultSection}>
-      <div className={styles.conversionResultContainer}>
-        <p>$1 USD = 0.7345 GBP</p>
-      </div>
-    </section>
+    <div className={styles.resultContainer}>
+      <p className={styles.mainResult}>
+        {amount} {fromCurrency} =
+      </p>
+      
+      <h2 className={styles.convertedAmount}>
+        {convertedAmount} {toCurrency}
+      </h2>
+      <p>
+        1 {fromCurrency} = {exchangeRate?.toFixed(4)} {toCurrency}
+      </p>
+    </div>
   )
 }
 

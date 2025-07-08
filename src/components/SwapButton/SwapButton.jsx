07ecdd28531712e04681ec8abb39ac7c3cc36e7a
Harson <hassandaf07@gmail.com>
2025-07-08 13:32:31 +0100

    implement swap button

--- src/components/SwapButton/SwapButton.jsx
+++ src/components/SwapButton/SwapButton.jsx
@@ -1,12 +1,19 @@
 import styles from './SwapButton.module.css'
 
-function SwapButton() {
+function SwapButton({
+  onClick
+}) {
+
+
   return(
-    <section className={styles.swapButtonSection}>
-      <div className={styles.swapButtonContainer}>
-        <button>Convert Currency</button>
-      </div>
-    </section>
+    <button className={styles.swapButton} onClick={onClick} aria-label='Swap currencies'>
+       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6.99 11L3 15L6.99 19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M3 15H21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M17.01 13L21 9L17.01 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M21 9H3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      </svg>
+    </button>
   )
 }
 

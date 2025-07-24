eb1f7e5cdfb33db5ce438898975b5035efd4f8ae
Harson <hassandaf07@gmail.com>
2025-07-24 13:09:42 +0100

    style navbar to show active states

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 function NavBar() {
     return(
       <nav className={styles.navBar}>
         <ul>
-            <li><Link to='/' className={styles.active}>Converter</Link></li>
-            <li><Link to='/rates' className={styles.active}>Live Rates</Link></li>
-            <li><Link to='/history'>Rate History</Link></li>
+            <li><NavLink to='/' end className={({ isActive }) => isActive ? styles.active : undefined}>Converter</NavLink></li>
+
+            <li><NavLink to='/rates' end className={({ isActive }) => isActive ? styles.active : undefined}>Live Rates</NavLink></li>
+
+            <li><NavLink to='/history' end className={({ isActive }) => isActive ? styles.active : undefined}>Rate History</NavLink></li>
         </ul>
       </nav>  
     )

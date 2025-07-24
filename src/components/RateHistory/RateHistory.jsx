24fb7005562ee5375bab593b696ac6bc9d9d86fa
Harson <hassandaf07@gmail.com>
2025-07-24 13:50:34 +0100

    add ratehistory files and folder

---
+++ src/components/RateHistory/RateHistory.jsx
@@ -0,0 +1,9 @@
+import styles from './RateHistory.module.css';
+
+function RateHistory() {
+    return(
+        <div className={styles.ratesContainer}></div>
+    )
+}
+
+export default RateHistory;

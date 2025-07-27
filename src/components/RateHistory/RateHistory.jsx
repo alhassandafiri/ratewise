95714e8fef626a1cb4863012446dd9cd8bd93856
Harson <hassandaf07@gmail.com>
2025-07-27 12:56:35 +0100

    import recharts

--- src/components/RateHistory/RateHistory.jsx
+++ src/components/RateHistory/RateHistory.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+import {
+LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
+} from 'recharts';
+
 import styles from './RateHistory.module.css';
 
 function RateHistory() {

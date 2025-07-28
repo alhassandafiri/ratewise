b76ae86eafca8f9fd628e01e135175df2b10eb0a
Harson <hassandaf07@gmail.com>
2025-07-28 12:52:53 +0100

    add currencymeta to currencyutils

--- src/utils/currencyUtils.js
+++ src/utils/currencyUtils.js
@@ -75,3 +75,36 @@ export const popularCurrencies = [
   'HUF', // Hungarian Forint
   'SAR'  // Saudi Riyal 
 ];
+
+export const currencyMeta = {
+  USD: { country: 'United States', code: 'USD' },
+  EUR: { country: 'Eurozone', code: 'EUR' },
+  JPY: { country: 'Japan', code: 'JPY' },
+  GBP: { country: 'United Kingdom', code: 'GBP' },
+  CNY: { country: 'China', code: 'CNY' },
+  AUD: { country: 'Australia', code: 'AUD' },
+  CAD: { country: 'Canada', code: 'CAD' },
+  CHF: { country: 'Switzerland', code: 'CHF' },
+  HKD: { country: 'Hong Kong', code: 'HKD' },
+  SGD: { country: 'Singapore', code: 'SGD' },
+  SEK: { country: 'Sweden', code: 'SEK' },
+  KRW: { country: 'South Korea', code: 'KRW' },
+  NOK: { country: 'Norway', code: 'NOK' },
+  NZD: { country: 'New Zealand', code: 'NZD' },
+  INR: { country: 'India', code: 'INR' },
+  MXN: { country: 'Mexico', code: 'MXN' },
+  TWD: { country: 'Taiwan', code: 'TWD' },
+  ZAR: { country: 'South Africa', code: 'ZAR' },
+  BRL: { country: 'Brazil', code: 'BRL' },
+  DKK: { country: 'Denmark', code: 'DKK' },
+  THB: { country: 'Thailand', code: 'THB' },
+  MYR: { country: 'Malaysia', code: 'MYR' },
+  IDR: { country: 'Indonesia', code: 'IDR' },
+  ILS: { country: 'Israel', code: 'ILS' },
+  AED: { country: 'United Arab Emirates', code: 'AED' },
+  TRY: { country: 'Turkey', code: 'TRY' },
+  PLN: { country: 'Poland', code: 'PLN' },
+  CZK: { country: 'Czech Republic', code: 'CZK' },
+  HUF: { country: 'Hungary', code: 'HUF' },
+  CLP: { country: 'Chile', code: 'CLP' },
+};
